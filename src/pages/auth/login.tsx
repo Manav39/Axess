a72f017abf62aaeb720ad009a826d937b067b4bb
Manav39 <Manav39>
2023-04-29 19:44:10 +0530

    changes in login

--- src/pages/auth/login.tsx
+++ src/pages/auth/login.tsx
@@ -1,45 +1,58 @@
-import React from "react";
-import {
-  Container,
-  Card,
-  Row,
-  Text,
-  Col,
-  Spacer,
-  Image,
-  Grid,
-  Input,
-  Button,
-  Avatar,
-} from "@nextui-org/react";
-import { FaUser , FaLock} from "react-icons/fa";
+import React, { useState } from "react";
+import { Container, Row, Text, Spacer, Input, Button } from "@nextui-org/react";
+import { FaUser, FaLock } from "react-icons/fa";
+import { GrOrganization } from "react-icons/gr";
 const login = () => {
+  const [userName, setUserName] = useState("");
+  const [password, setPassword] = useState("");
+  const [orgId, setOrgId] = useState("");
   return (
     <>
       <Container
         display="flex"
         justify="center"
         css={{
-        marginTop: "11rem",
-        border: "0.1rem solid black",
-        width:'25rem',
-        padding: "2rem",
-         }}
+          marginTop: "11rem",
+          boxShadow: "0 0 10px rgba(0, 0, 0, 0.5);",
+          borderRadius: "1rem",
+          width: "25rem",
+          padding: "2rem",
+        }}
       >
-        <Text h2 style={{ marginBottom: "2.5rem" }}>
+        <Text h2 style={{ marginBottom: "2.5rem", color: "blue" }}>
           Login
         </Text>
-        
         <Row justify="center">
-            <FaUser size={40} />
-        <Spacer x={1} />
-          <Input placeholder="Enter Username" width="30rem" />
-        </Row>
-        <Spacer y={2} />
-        <Row justify="center">
-        <FaLock size={40} />
+          <GrOrganization size={42} />
           <Spacer x={1} />
-          <Input placeholder="Enter Password" width="30rem" />
+          <Input
+            placeholder="Enter Organization ID"
+            width="30rem"
+            value={orgId}
+            onChange={(e) => setOrgId(e.target.value)}
+          />
+        </Row>
+        <Spacer x={2} />
+        <Row justify="center">
+          <FaUser size={40} />
+          <Spacer x={1} />
+          <Input
+            placeholder="Enter Username"
+            width="30rem"
+            value={userName}
+            onChange={(e) => setUserName(e.target.value)}
+          />
+        </Row>
+        <Spacer y={1} />
+        <Row justify="center">
+          <FaLock size={40} />
+          <Spacer x={1} />
+          <Input
+            placeholder="Enter Password"
+            width="30rem"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
         </Row>
         <Spacer y={2} />
         <Row justify="center">
