e16c90d84718581037df48f9f10edcae0d4f15b7
arnitdo <arnav.nitin.deo@gmail.com>
2023-04-29 19:46:45 +0530

    Add editorconfig file

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,10 @@
-import type { AppProps } from 'next/app'
-import { NextUIProvider } from '@nextui-org/react';
+import type {AppProps} from 'next/app'
+import {NextUIProvider} from '@nextui-org/react';
 
-export default function App({ Component, pageProps }: AppProps) {
-  return (
-    <NextUIProvider>
-    <Component {...pageProps} />
-  </NextUIProvider>
-  )
+export default function App({Component, pageProps}: AppProps) {
+	return (
+		<NextUIProvider>
+			<Component {...pageProps} />
+		</NextUIProvider>
+	)
 }
