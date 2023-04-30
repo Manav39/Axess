4bcb59cf25a27e7cd6f4c10803da68f61fe3e300
arnitdo <arnav.nitin.deo@gmail.com>
2023-04-30 14:45:51 +0530

    Final

--- src/pages/orgs/[orgId]/access.tsx
+++ src/pages/orgs/[orgId]/access.tsx
@@ -1,6 +1,66 @@
-import { useRouter } from "next/router";
-export default function Access() {
-	const router = useRouter();
-	console.log(router.query);
-	return <h1></h1>;
+import {useRouter} from "next/router";
+import {useContext} from "react";
+import {AuthContext} from "@/pages/_app";
+import {makeAPIRequest} from "@/utils/apiHandler";
+import {AccessPointRequestBody, AccessPointRequestParams} from "@/utils/types/apiRequests";
+import {APIResponse} from "@/utils/types";
+import {Button, Container} from "@nextui-org/react";
+
+export default function AccessDevice(): JSX.Element {
+	const router = useRouter()
+	const {orgId, deviceId, deviceName} = router.query
+	if (!deviceId || !deviceName) {
+	}
+	
+	const authCtx = useContext(AuthContext)
+	
+	const makeReq = async (action: `CHECK_${"IN" | "OUT"}`) => {
+		const {isSuccess, data} = await makeAPIRequest<APIResponse, AccessPointRequestBody, AccessPointRequestParams>({
+			requestMethod: "POST",
+			endpointPath: "/api/orgs/:orgId/access",
+			queryParams: {
+				orgId: orgId as string
+			},
+			bodyParams: {
+				deviceId: deviceId as string,
+				actionType: action,
+				accessTimestamp: (new Date()).toString()
+			}
+		})
+		if (isSuccess && data) {
+			const {requestStatus} = data
+			if (requestStatus === "SUCCESS") {
+			}
+		}
+	}
+	
+	return (
+		<Container
+			css={{
+				width: "100vw",
+				height: "90vh"
+			}}
+			display={"flex"}
+			justify={"center"}
+			alignItems={"center"}
+			direction={"column"}
+			css={{
+				gap: 24
+			}}
+		>
+			<h2>
+				{deviceName}
+			</h2>
+			<Button
+				onClick={() => makeReq("CHECK_IN")}
+			>
+				Check In
+			</Button>
+			<Button
+				onClick={() => makeReq("CHECK_OUT")}
+			>
+				Check Out
+			</Button>
+		</Container>
+	)
 }
--- src/pages/orgs/[orgId]/devices/[deviceId].tsx
+++ src/pages/orgs/[orgId]/devices/[deviceId].tsx
@@ -1,10 +1,13 @@
-import { NavbarControl, UserPermissionLevel } from "@/utils/types";
-import { GetServerSideProps } from "next";
-import { useEffect, useState } from "react";
+import {NavbarControl, UserPermissionLevel} from "@/utils/types";
+import {GetServerSideProps} from "next";
+import {useContext, useEffect, useState} from "react";
 import QRCode from "react-qr-code";
-import { AuthContext } from "@/pages/_app";
-import { useContext } from "react";
-import { Container, Text } from "@nextui-org/react";
+import {AuthContext} from "@/pages/_app";
+import {Container, Text} from "@nextui-org/react";
+import {makeAPIRequest} from "@/utils/apiHandler";
+import {GetDeviceResponse} from "@/utils/types/apiResponses";
+import {GetDeviceRequestBody, GetDeviceRequestParams} from "@/utils/types/apiRequests";
+
 export type DeviceQRPageProps = {
 	orgId: string;
 	deviceData: {
@@ -20,17 +23,41 @@ type DeviceQRCodePageQuery = {
 };
 
 export const getServerSideProps: GetServerSideProps<DeviceQRPageProps, DeviceQRCodePageQuery> = async (ctx) => {
-	const { orgId, deviceId } = ctx.params!;
-
-	return {
-		props: {
+	const {orgId, deviceId} = ctx.params!;
+	
+	const {isSuccess, data} = await makeAPIRequest<GetDeviceResponse, GetDeviceRequestBody, GetDeviceRequestParams>({
+		endpointPath: "/api/orgs/:orgId/devices/:deviceId",
+		requestMethod: "GET",
+		queryParams: {
 			orgId: orgId,
-			deviceData: {
-				deviceUUID: deviceId,
-				deviceName: "Some Device",
-				permissionLevel: UserPermissionLevel.USER,
-			},
+			deviceId: deviceId
 		},
+		ssrContext: ctx
+	})
+	
+	if (isSuccess && data) {
+		const {requestStatus} = data
+		if (requestStatus === "SUCCESS") {
+			const {
+				deviceData: {
+					deviceId, deviceName, permissionLevel
+				}
+			} = data
+			return {
+				props: {
+					orgId: orgId,
+					deviceData: {
+						deviceUUID: deviceId,
+						deviceName: deviceName,
+						permissionLevel: UserPermissionLevel.USER,
+					}
+				}
+			}
+		}
+	}
+	
+	return {
+		notFound: true
 	};
 };
 
@@ -42,7 +69,7 @@ export default function DeviceQRPage(props: DeviceQRPageProps & NavbarControl) {
 	//[7:36 am, 30/04/2023] Arnav Deo: &deviceName=<Human Friendly Device Name></Human>
 	const {
 		orgId,
-		deviceData: { deviceUUID, deviceName, permissionLevel },
+		deviceData: {deviceUUID, deviceName, permissionLevel},
 	} = props;
 	useEffect(() => {
 		props.setShowNavbar(false);
@@ -51,13 +78,13 @@ export default function DeviceQRPage(props: DeviceQRPageProps & NavbarControl) {
 			props.setShowNavbar(true);
 		};
 	}, []);
-
+	
 	return (
 		<>
-			<Container display="flex" direction="row" justify="center" style={{ marginTop: "200px" }}>
-				<QRCode value={qrValue} />
+			<Container display="flex" direction="row" justify="center" style={{marginTop: "200px"}}>
+				<QRCode value={qrValue}/>
 			</Container>
-			<Text h2 style={{ textAlign: "center" }}>
+			<Text h2 style={{textAlign: "center"}}>
 				{props.deviceData.deviceName}
 			</Text>
 		</>
