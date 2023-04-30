67288f6738cffe3e1c459f35b4976ea92607dad2
arnitdo <arnav.nitin.deo@gmail.com>
2023-04-30 10:57:24 +0530

    Refactor

---
+++ src/pages/auth/superuser_login.tsx
@@ -0,0 +1,159 @@
+import React, {useCallback, useContext, useState} from "react";
+import {Button, Container, Input, Row, Spacer, Text} from "@nextui-org/react";
+import {FaLock, FaUser} from "react-icons/fa";
+import {GrSecure} from "react-icons/gr";
+import {makeAPIRequest} from "@/utils/apiHandler";
+import {LoginAdminRequestBody} from "@/utils/types/apiRequests";
+import {LoginUserResponse} from "@/utils/types/apiResponses";
+import {AuthContextType} from "@/utils/types";
+import {AuthContext} from "../_app";
+import {useRouter} from "next/router";
+
+export default function LoginPage() {
+	const router = useRouter();
+	const AuthCtx = useContext<AuthContextType>(AuthContext);
+	const [userName, setUserName] = useState("");
+	const [password, setPassword] = useState("");
+	const [adminSecret, setAdminSecret] = useState("");
+	const [invalid, setInvalid] = useState({
+		userId: false,
+		userPass: false,
+	});
+	const [missing, setMissing] = useState({
+		userId: false,
+		userPass: false,
+	});
+	const attemptLogin = useCallback(async () => {
+		const {isSuccess, isError, code, data, error} = await makeAPIRequest<
+			LoginUserResponse,
+			LoginAdminRequestBody
+		>({
+			requestMethod: "POST",
+			endpointPath: "/api/sudo/login",
+			bodyParams: {
+				userId: userName,
+				userPass: password,
+				adminSecret: adminSecret
+			}
+		});
+		if (isError && error) {
+			console.error(error);
+			return;
+		}
+		if (isSuccess && data) {
+			const {requestStatus} = data;
+			if (requestStatus === "SUCCESS") {
+				const {userId, permissionLevel} = data;
+				AuthCtx.updateAuthData({
+					isAuthenticated: true,
+					userId: userId,
+					permissionLevel: permissionLevel,
+					orgId: undefined,
+					tokenType: "SUDO"
+				});
+				setInvalid((invalidData) => {
+					return {
+						...invalidData,
+						orgId: false,
+						userId: false,
+						userPass: false,
+					};
+				});
+				router.push("/");
+			}
+			if (requestStatus === "ERR_INVALID_QUERY_PARAMS") {
+				setInvalid((invalidData) => {
+					return {
+						...invalidData,
+						orgId: true,
+					};
+				});
+			}
+			if (requestStatus === "ERR_INVALID_BODY_PARAMS") {
+				setInvalid((invalidData) => {
+					return {
+						...invalidData,
+						userId: true,
+						userPass: true,
+					};
+				});
+			}
+			if (requestStatus === "ERR_MISSING_BODY_PARAMS") {
+				setMissing((missingParams) => {
+					return {
+						...missingParams,
+						userId: true,
+						userPass: true,
+					};
+				});
+			}
+			if (requestStatus === "ERR_MISSING_QUERY_PARAMS") {
+				setMissing((missingParams) => {
+					return {
+						...missingParams,
+						orgId: true,
+					};
+				});
+			}
+		}
+	}, [userName, adminSecret, password]);
+	
+	return (
+		<>
+			<Container
+				display="flex"
+				justify="center"
+				css={{
+					marginTop: "11rem",
+					boxShadow: "0 0 10px rgba(0, 0, 0, 0.5);",
+					borderRadius: "1rem",
+					width: "25rem",
+					padding: "2rem",
+				}}
+			>
+				<Text h2 style={{marginBottom: "2.5rem", color: "blue"}}>
+					Login
+				</Text>
+				<Row justify="center">
+					<GrSecure size={42}/>
+					<Spacer x={1}/>
+					<Input
+						placeholder="Enter Admin Secret"
+						width="30rem"
+						value={adminSecret}
+						onChange={(e) => setAdminSecret(e.target.value)}
+					/>
+				</Row>
+				<Spacer x={2}/>
+				<Row justify="center">
+					<FaUser size={40}/>
+					<Spacer x={1}/>
+					<Input
+						placeholder="Enter Username"
+						width="30rem"
+						value={userName}
+						onChange={(e) => setUserName(e.target.value)}
+					/>
+				</Row>
+				<Spacer y={1}/>
+				<Row justify="center">
+					<FaLock size={40}/>
+					<Spacer x={1}/>
+					<Input
+						placeholder="Enter Password"
+						type="password"
+						width="30rem"
+						value={password}
+						onChange={(e) => setPassword(e.target.value)}
+					/>
+				</Row>
+				<Spacer y={2}/>
+				<Row justify="center">
+					<Button style={{width: "35rem"}} onClick={attemptLogin}>
+						Login
+					</Button>
+				</Row>
+			</Container>
+		</>
+	);
+}
